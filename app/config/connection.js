bd6217aa5efb5d7e7989bd6ad56f755608aafd20
estivensal7 <estivensal7@gmail.com>
2019-02-20 23:31:07 -0500

    ..

--- app/config/connection.js
+++ app/config/connection.js
@@ -2,22 +2,27 @@
 const Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/config.json')[env];
+// Construct variable
+let sequelize;
 
-if (process.env.JAWS_DB) {
-        var sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-        var sequelize = new Sequelize("stacheBarbers", "root", "root1234", {
-                host: "localhost",
-                dialect: 'mysql',
-                operatorsAliases: false,
-                
-                pool: {
-                        max: 5,
-                        min: 0,
-                        acquire: 30000,
-                        idle: 10000
-                }
-        });
+// Create sequelize connection
+// If deployed on heroku, use JAWSDB database
+if (process.env.JAWSDB_URL) {
+	sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+// Use local host
+else {
+	sequelize = new Sequelize("stacheBarbers", "root", "root1234", {
+		host: "localhost",
+		dialect: "mysql",
+		port: 3306,
+		pool: {
+			max: 5,
+			min: 0,
+			idle: 10000
+		}
+	});
 }
 
+// export sequlize instance
 module.exports = sequelize;
