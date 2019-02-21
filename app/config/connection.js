73191ab67ea485f25672b7b00cb3cb7939d149fb
estivensal7 <estivensal7@gmail.com>
2019-02-20 23:02:43 -0500

    fixing jawsdb url variable

--- app/config/connection.js
+++ app/config/connection.js
@@ -4,7 +4,7 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/config.json')[env];
 
 if (process.env.JAWS_DB) {
-        var sequelize = new Sequelize(process.env.JAWS_DB);
+        var sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
         var sequelize = new Sequelize(config.database, config.username, config.password, config, {
                 host: config.host,
